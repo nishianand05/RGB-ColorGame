567d8c7f2a4b1cda5d768e10cc77fe1da5cab89c
nishianand05 <nishianand.05@gmail.com>
2021-07-15 20:45:39 +0530

    PWA Feature

--- colorgamescript.js
+++ colorgamescript.js
@@ -1,6 +1,15 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/service-worker.js');
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(reg => {
+                console.log('Registration succeeded. Scope is ' + reg.scope);
+            })
+            .catch(registrationError => {
+                console.log('SW registration failed: ', registrationError);
+            });
+    });
 }
+
 
 // Selecting
 var squares = document.querySelectorAll(".square");
